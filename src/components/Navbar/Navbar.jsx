db1bde060a70eac48cb7385addd8a3e7a2d732fb
Kenric <kenric.chui@gmail.com>
2024-06-25 22:55:34 +1000

    second push

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      
+      <div className="navbar--left">
+        <img src={logo} alt="" />
+      </div>
+      <div className="navbar--right"></div>
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
   return (
     <div className='home'>
       <Navbar/>
+      hello
     </div>
   )
 }
