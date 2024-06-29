efce7f7d786d32016b6bd7fec3e0b34cd9183b40
Kenric <kenric.chui@gmail.com>
2024-06-29 20:21:30 +1000

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,6 +14,21 @@ const Footer = () => {
         <img src={twitter_icon} alt="" />
         <img src={youtube_icon} alt="" />
       </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Centre</li>
+        <li>Gift Cards</li>
+        <li>Media Centre</li>
+        <li>Investor Relations</li>
+        <li>Jobs</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Legal Notice</li>
+        <li>Cookie Preferences</li>
+        <li>Corporate Information</li>
+        <li>Contact Us</li>
+      </ul>
+      <p className='copyright__text'>&copy; 1997-2023 Netflix, Inc.</p>
     </div>
   )
 }
