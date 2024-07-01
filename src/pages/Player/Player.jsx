3caacf050bec06135010ecee8c0cddd7ebcd8604
Kenric <kenric.chui@gmail.com>
2024-07-01 22:39:06 +1000

    player

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,12 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useParams } from 'react-router-dom'
 
-const PLyaer = () => {
+const Player = () => {
+
+  const {id} = useParams();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: ""
+  })
+
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYTQxNTQyMTA0NTQ5MmE1YjIyZWU5ZWEwMzAzOTdmZiIsIm5iZiI6MTcxOTY2NDM2Ny40NTE1OTEsInN1YiI6IjY2N2ZmZTFjZmMwYWViZGQwZWU1ZGZhZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.F2IF3QIsr5ZkJL_AM_aMRWtkvJxmA3h37fHyxPoRfT8'
+    }
+  };
+  
+  useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results[0]))
+    .catch(err => console.error(err));
+  },[])
+  
+
+ 
+
   return (
     <div className='player'>
-      
+      <img src={back_arrow_icon} alt="" />
+      <iframe width='90%' height='90%'
+      src={`https://www.youtube.com/embed/${apiData.key}`}
+      title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <div className="player__info">
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
     </div>
   )
 }
 
-export default PLyaer
+export default Player
